d96c209fbeff242993a773c6841155729f1b8021
rafcio96@gmail.com <rafcio96@gmail.com>
2018-04-25 00:30:28 +0200

    Coś z GUI

--- ProjektTechniki/MainWindow.xaml.cs
+++ ProjektTechniki/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ProjektTechniki
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
